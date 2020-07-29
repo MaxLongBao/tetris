04e48d0fba684417efb99af0f2925c63be3a4be5
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2020-07-29 01:49:20 +0100

    Updates

---
+++ precache-manifest.a6ab4039e12c46a773ec7ea7adcac84b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "504fc6fbf95d08371c5e500802600baf",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "ce0ce34bf9b6622b5247",
+    "url": "/tetris/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "904a91619bacd49dae7e",
+    "url": "/tetris/static/js/2.62e1afba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tetris/static/js/2.62e1afba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce0ce34bf9b6622b5247",
+    "url": "/tetris/static/js/main.fbe554f2.chunk.js"
+  },
+  {
+    "revision": "c179afea1aa9726cb16b",
+    "url": "/tetris/static/js/runtime-main.0d8747ab.js"
+  }
+]);
